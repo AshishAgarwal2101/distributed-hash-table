aa6a0c963dc63502305b51352e015b93952ca1ae
Ashish <ashish21.01agarwal@gmail.com>
2022-11-12 11:46:42 -0800

    Remote call using gRPC

--- index.ts
+++ index.ts
@@ -18,8 +18,9 @@ class ChordNode {
     nodeDetails: NodeDetails;
     successors: Array<NodeDetails> = [];
     predecessor: NodeDetails = NULL_NODE_DETAILS;
+    fingers: Array<NodeDetails> = [];
 
-    constructor(host, port){
+    constructor(host: string, port: number){
         this.nodeDetails = {
             id: getHash(`$host:$port`),
             host: host,
@@ -27,14 +28,38 @@ class ChordNode {
         }
     }
 
-    isItMyNode(nodeDetails: NodeDetails) : boolean {
+    isItMyNode(nodeDetails: NodeDetails): boolean {
         if(nodeDetails.id == this.nodeDetails.id) return true;
         return false;
     }
-    
-    async joinCluster() : Promise<void> {
+
+    async findSuccessor(id: number): Promise<NodeDetails> {
+        if(id > this.nodeDetails.id && this.successors.length > 0 && id < this.successors[0].id){
+            return this.successors[0];
+        }
+
+        let closestPrecedingNodeForId = await this.closestPrecedingNode(id);
+        return await this.findSuccessorRemote(id, closestPrecedingNodeForId);
+    }
+
+    async findSuccessorRemote(id: number, closestPrecedingNode: NodeDetails): Promise<NodeDetails> {
+        return null;
+    }
+
+    async closestPrecedingNode(id: number): Promise<NodeDetails> {
+        for(let i=this.fingers.length-1; i>=0; i--){
+            let fingerId = this.fingers[i].id;
+            if(fingerId > this.nodeDetails.id && fingerId < id){
+                return this.fingers[i];
+            }
+        }
+        return this.nodeDetails;
+    }
+
+    async joinCluster(): Promise<void> {
         let bitSize = HASH_NUM_OF_BITS;
     };
+
 }
 
 export default ChordNode;
