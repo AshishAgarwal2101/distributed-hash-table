c49d6f5e7058bdb257e4b2e97d737345cc9100bf
Ashish <ashish21.01agarwal@gmail.com>
2022-11-29 12:20:55 -0800

    logger for client connection failure

--- remote/RemoteClient.ts
+++ remote/RemoteClient.ts
@@ -3,5 +3,11 @@ import grpcCaller from "grpc-caller";
 let PROTO_PATH = __dirname + '/protos/route_guide.proto';
 
 export const getClient = (host, port) => {
-    return grpcCaller(host + ":" + port, PROTO_PATH, "RouteGuide");
+    try{
+        let client = grpcCaller(host + ":" + port, PROTO_PATH, "RouteGuide");
+        return client;
+    } catch(e) {
+        console.log(`Failed to connect to client with ${host} and ${port}`);
+        throw e;
+    }
 };
