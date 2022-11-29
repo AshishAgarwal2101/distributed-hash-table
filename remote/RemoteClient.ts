100e2ac86449f71b10c816da4bcf89d2b6e660f9
Ashish <ashish21.01agarwal@gmail.com>
2022-11-29 14:50:26 -0800

    grpc options

--- remote/RemoteClient.ts
+++ remote/RemoteClient.ts
@@ -1,10 +1,11 @@
 import grpcCaller from "grpc-caller";
+import grpc from "grpc";
 
 let PROTO_PATH = __dirname + '/protos/route_guide.proto';
 
 export const getClient = (host, port) => {
     try{
-        let client = grpcCaller(host + ":" + port, PROTO_PATH, "RouteGuide").withGrpcOptions({ "grpc.keepalive_time_ms": 10000,  "grpc.keepalive_timeout_ms": 5000});
+        let client = grpcCaller(host + ":" + port, PROTO_PATH, "RouteGuide", undefined, { "grpc.keepalive_time_ms": 10000,  "grpc.keepalive_timeout_ms": 5000});
         return client;
     } catch(e) {
         console.log(`Failed to connect to client with ${host} and ${port}`);
